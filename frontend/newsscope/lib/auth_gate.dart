6794edf82e41ff8f11ff65d9964cdd7e3d9c1bae
Christopher Noblett <C22454222@mytudublin.ie>
2025-11-06 19:13:53 +0000

    Making firebase and flutter frontend sign in, authorise and sign up

---
+++ frontend/newsscope/lib/auth_gate.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'sign_in_screen.dart';
+import 'home_screen.dart';
+
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return const HomeScreen();
+        } else {
+          return const SignInScreen();
+        }
+      },
+    );
+  }
+}
---
+++ frontend/newsscope/lib/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text("Welcome ${user?.email ?? user?.displayName}!"),
+          ElevatedButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            child: const Text("Sign out"),
+          ),
+        ]),
+      ),
+    );
+  }
+}
